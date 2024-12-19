51e1debe8007ab66679e86c43e9bf08c55da388b
IvanB <jurislocsdev@gmail.com>
2024-12-19 09:42:44 +0200

    cannot find the auth package

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AUTH } from '@app/comon';
+import { UserProductsModule } from './user-products/user-products.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions> (){
-    Auth
-  }
+  const app = await NestFactory.createMicroservice<MicroserviceOptions> (
+    UserProductsModule,
+  {
+    transport: Transport.GRPC,
+    options: {
+      protoPath: join(__dirname, '../auth.proto'),
+      package: AUTH
+    },
+  },
+);
+await app.listen();
 }
 bootstrap();
